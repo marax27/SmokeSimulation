8e1cac162c6cafe1722f16ef6e391aa6ccc73b59
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2019-05-23 23:19:04 +0200

    Fixed camera movement acceleration/decceleration

--- OfSmokeSim/src/ofApp.cpp
+++ OfSmokeSim/src/ofApp.cpp
@@ -2,17 +2,17 @@
 
 //--------------------------------------------------------------
 int x = 30, y = 30, z = 30;
-int fps = 4;
+int fps = 15;
 double dx = 0.5;
 SmokeSolver smokeSolver(x, y, z);
 BoundaryBox boundaryBox(x * dx, y * dx, z * dx);
 
 void ofApp::setup() {
 	ofSetFrameRate(fps);
-	ofEnableDepthTest();
-	ofEnableLighting();
+	//ofEnableLighting();
 	ofBackground(0, 0, 0);
-	light.setup();
+	//light.setup();
+	//glEnable(GL_DEPTH_TEST);
 
 	smokeSolver.setBuoyancy(0.3);
 	smokeSolver.setDt(0.01);
@@ -34,23 +34,18 @@ void ofApp::draw() {
 	auto field = smokeSolver.getDensityField();
 	
 	cam.begin();
-	light.enable();
+	//light.enable();
 
 	cameraControl();
-	light.setPosition(cam.getPosition());
+	//light.setPosition(cam.getPosition());
 
 	boundaryBox.draw();
-	
-	glDisable(GL_DEPTH_TEST); 
-	glCullFace(GL_FRONT);
-	//glEnable(GL_BLEND);
-	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	
 	shader.begin();
 	drawSmoke(field);
 	shader.end();
 
-	light.disable();
+	//light.disable();
 	cam.end();
 	
 }
@@ -64,9 +59,9 @@ void ofApp::keyPressed(int key){
 	if(key == 's')
 		cameraOrbitLatitude += 10.0f;
 	if(key == 'd')
-		cameraOrbitLongitude += 5.0f;
+		cameraOrbitLongitudeDPS += 10.0f;
 	if(key == 'a')
-		cameraOrbitLongitude -= 5.0f;
+		cameraOrbitLongitudeDPS -= 10.0f;
 }
 
 //--------------------------------------------------------------
@@ -123,24 +118,21 @@ void ofApp::cameraControl() {
 	cam.setTarget(ofPoint(dx * x / 2, dx * y / 2, dx * z / 2));
 
 	if(!pauseRotation) {
-		cam.orbitDeg(cameraOrbitLongitude * ofGetElapsedTimef(), 
+		cam.orbitDeg(currentCameraLongitude, 
 					cameraOrbitLatitude, 
 					30, 
 					ofPoint(dx * x / 2, dx * y / 2, dx * z / 2));
-					//cout << cameraOrbitLongitude * ofGetElapsedTimef() << " " << cameraOrbitLatitude << endl;
 	}
+	currentCameraLongitude += cameraOrbitLongitudeDPS / fps;
 }
 
 void ofApp::drawSmoke(Field3D &field) {
-	double currentHighestDensity = highestDensity;
 	double density;
 	for (int i = 1; i < field.XLast() - 1; ++i) {
 		for (int j = 1; j < field.YLast() - 1; ++j) {
 			for (int k = 1; k < field.ZLast() - 1; ++k) {
 				density = field(i, j, k);
 				if (density != 0) {
-					highestDensity = highestDensity < density ? density : highestDensity;
-					shader.setUniform1f("density", density);
 					shader.setUniform1f("alpha", density);
 					ofDrawBox(i * dx, j * dx, k * dx, dx); // draw smoke particle
 				}
